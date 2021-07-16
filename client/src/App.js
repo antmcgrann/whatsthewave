6dbb2963f54c3779019abac58d0e0cff64a50c0f
Andrew Walek <atwalek@gmail.com>
2021-07-16 17:28:37 -0400

    Client side now shows an interactive google maps

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/*
 const App = () => {
     return (
         <div>
@@ -9,3 +10,33 @@ const App = () => {
 }
 
 export default App;
+*/
+
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+};
+
+export class MapContainer extends React.Component {
+    render() {
+        return (
+            <Map
+            google = {this.props.google}
+            zoom = {14}
+            style = {mapStyles}
+            initialCenter = {
+                {
+                lat: 73.6789,
+                lng: 42.7298
+                }
+            }
+            />
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDdIBVvzNbyOpiU1qvyJiWbAW6CFm1KoQs'
+})(MapContainer);
