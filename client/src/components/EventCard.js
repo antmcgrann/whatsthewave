bf1b8794647c1808319b92d75956c72db0ad572c
asandonato <72477245+asandonato@users.noreply.github.com>
2021-08-02 12:39:33 -0400

    Update EventCard.js

---
+++ client/src/components/EventCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import eventdata from '../events.json';
+
+const EventCard() = () =>{
+	return(
+		<>
+			<div ClassName = "eventdata">
+				{event.data.map((data, key) =>{
+					return(
+						<div key={key}>
+							{data.title + ":" + data.tags}
+							{data.creator}
+							{data.date}
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
+
+export default EventCard;
