b133f38a738f4836c4cd6b44781b94ba34ccc322
Andrew Walek <atwalek@gmail.com>
2021-08-03 12:35:11 -0400

    Storing events for user accounts

--- server/models/accountData.js
+++ server/models/accountData.js
@@ -5,9 +5,36 @@ const accountSchema = mongoose.Schema ({
     password: String,
     bio: String,
     email: String,
-    eventsHosted: [String],
-    eventsRSVP: [Event],
-    eventsAttended: [String]
+    eventsHosted: [ {
+        title: String,
+        lat: Number,
+        lng: Number,
+        desc: String,
+        creator: String,
+        tags: [String],
+        rsvp: [String],
+        date: Date
+    } ],
+    eventsRSVP: [ { 
+        title: String,
+        lat: Number,
+        lng: Number,
+        desc: String,
+        creator: String,
+        tags: [String],
+        rsvp: [String],
+        date: Date
+    } ],
+    eventsAttended: [ {
+        title: String,
+        lat: Number,
+        lng: Number,
+        desc: String,
+        creator: String,
+        tags: [String],
+        rsvp: [String],
+        date: Date
+    } ]
 })
 
 const AccountData = Mongoose.model('AccountData', accountSchema);
@@ -15,6 +42,17 @@ const AccountData = Mongoose.model('AccountData', accountSchema);
 const account1 = new AccountData({username: 'wtwAdmin', password: 'password', bio: 'The creators of Whats the Wave', 
     email: 'whatsthewave@gmail.com', eventsHosted: ['SDD party'], eventsRSVP: ['SDD party part 2'], eventsAttended: ['SDD Lecture', 'SDD party'] });
 
+const account2 = new AccountData({username: 'wtwAdmin', password: 'password', bio: 'The creators of Whats the Wave', 
+    email: 'whatsthewave@gmail.com', eventsHosted: [ {
+        title: 'SDD party',
+        lat: 42.7248,
+        lng: -73.6918,
+        desc: 'End of the semester party! Celebrate with cake!',
+        creator: 'wtwAdmin',
+        tags: ['Get-together', 'Food'],
+        rsvp: ['AndrewW27', 'AnthonyM30', 'AudreyS2', 'WillW20', 'JustinK45'],
+        date: '2021-08-20'
+    }], eventsRSVP: ['SDD party part 2'], eventsAttended: ['SDD Lecture', 'SDD party'] });
 
 account1.save(function (err) {
     if (err) return handleError(err); 
