885c1bd02c0efc208a25b5fd1000f357dcd6edc3
pathfold <52586263+pathfold@users.noreply.github.com>
2021-08-07 12:31:58 -0400

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Header from './components/Header/Header';
 import Landing from './pages/Landing/Landing';
+import CreateEvent from './pages/CreateEvent/CreateEvent'
 import EventManagement from './pages/EventManagement/EventManagement'
 import LogIn from './pages/LogIn/LogIn';
 
@@ -19,22 +20,25 @@ export default class App extends React.Component {
     }
 
     render(){
-        this.updateTitle = this.updateTitle.bind(this);
-        return (
-            <BrowserRouter>
-                <Header/>
-                <Switch>
-                    <Route exact path="/">
-                        <Landing updateTitle={this.updateTitle}/>
-                    </Route>
-                    <Route exact path="/event-management">
-                        <EventManagement updateTitle={this.updateTitle}/>
-                    </Route>
-                    <Route exact path="/login">
-                        <LogIn updateTitle={this.updateTitle}/>
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        );
+    this.updateTitle = this.updateTitle.bind(this);
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route exact path="/">
+                    <Landing updateTitle={this.updateTitle}/>
+                </Route>
+                <Route exact path="/event-management">
+                    <EventManagement updateTitle={this.updateTitle}/>
+                </Route>
+                <Route exact path="/login">
+                    <LogIn updateTitle={this.updateTitle}/>
+                </Route>
+                <Route exact path="/event-management/create">
+                    <CreateEvent />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
     }
 }
