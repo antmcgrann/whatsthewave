819326e29260b4c13255b940f8aee410d0e7f8c7
pathfold <52586263+pathfold@users.noreply.github.com>
2021-08-20 12:41:00 -0400

    error test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import eventRoutes from './routes/events.js';
 import accountRoutes from './routes/account.js';
 
 const app = express();
-const path = require('path');
 
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: "30mb", extended: true}));
@@ -15,16 +14,6 @@ app.use(cors());
 
 app.use('/events', eventRoutes);
 app.use('/accounts', accountRoutes);
-
-//serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  //set static folder, load static into build
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const CONNECTION_URL = 'mongodb+srv://whatsthewave-user:whatsthewave@cluster0.ayb4p.mongodb.net/whatsthewave?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
