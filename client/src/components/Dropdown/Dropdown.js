d1daa3369f8ae9e6d2c2414710121804e079cec2
pathfold <pathfolder@gmail.com>
2021-08-22 01:36:18 -0400

    added icons to dropdown menu, changed dropdown from absolute => fixed

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Dropdown.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCaretUp, faUserCircle, faCalendar, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default class Dropdown extends React.Component {
     constructor(props) {
@@ -30,9 +30,18 @@ export default class Dropdown extends React.Component {
                 {this.state.open && 
                     (<div class="dropdown-menu">
                         <ul class="dropdown-list">
-                            <li class="dropdown-item"><a>My Account</a></li>
-                            <li class="dropdown-item"><a onClick={this.handleClick} href="/event-management">Event Management</a></li>
-                            <li class="dropdown-item"><a style={{cursor:`pointer`}} onClick={this.props.handleLogout}>Log Out</a></li>
+                            <a class="dropdown-item">
+                                <FontAwesomeIcon icon={faUserCircle} class="dropdown-icon"/>
+                                <div>My Account</div>
+                            </a>
+                            <a class="dropdown-item" href="/event-management" onClick={this.handleClick}>
+                                <FontAwesomeIcon icon={faCalendar} class="dropdown-icon"/>
+                                <div>Event Management</div>
+                            </a>
+                            <a class="dropdown-item">
+                                <FontAwesomeIcon icon={faSignOutAlt} class="dropdown-icon"/>
+                                <div style={{cursor:`pointer`}} onClick={this.props.handleLogout}>Log Out</div>
+                            </a>
                         </ul>
                     </div>)}
             </div>
